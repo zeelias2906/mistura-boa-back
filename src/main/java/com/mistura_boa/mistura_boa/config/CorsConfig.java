d633812bcf0518c38bf87e9c23168fa7fd850a8f
zeelias2906 <js.elias27@hotmail.com>
2025-05-12 21:49:56 -0300

    dev

--- src/main/java/com/mistura_boa/mistura_boa/config/CorsConfig.java
+++ src/main/java/com/mistura_boa/mistura_boa/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://mistura-boa-back.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
